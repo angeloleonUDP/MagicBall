f02665b048a407619f8f767c6c6646f8ced5dc5d
Angelo Leon <aleon@mosaico.cl>
2021-05-06 23:10:53 -0400

    Segunda Modificacion

--- app/src/main/java/com/example/magicball/MainActivity.java
+++ app/src/main/java/com/example/magicball/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.magicball;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ImageButton Ball;
+    private TextView respuesta;
+    private String [] respuestaArray ={"No way","ask my mom", "Tal vez"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Referencia la imagen
+        Ball= findViewById(R.id.btn);
+        //referencia tecto
+        respuesta = findViewById(R.id.respuesta);
+
+        //proceso
+        Ball.setOnClickListener(this);
+        Toast.makeText(MainActivity.this, "Consulta tu suerte", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View v){
+        //creación metodo switch
+        switch(v.getId()){
+            case R.id.btn:
+
+                int rand = new Random().nextInt(respuestaArray.length);
+                respuesta.setText(respuestaArray[rand]);
+
+
+                break;
+        }
     }
 }
